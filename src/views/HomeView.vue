98d199cea38beb10bf8673f35816b089705196cb
sareum <rkdtkfma@gmail.com>
2022-08-13 22:11:15 +0900

    form component 분리

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,59 @@
 <script setup lang="ts">
-import TodoList from '../components/TodoList.vue'
+import { ref } from 'vue'
+
+import TodoSimpleForm from '../components/TodoSimpleForm.vue'
+
+export interface Item {
+  id: number
+  content: string
+  completed: boolean
+}
+
+const itemList = ref<Item[]>([])
+const itemStyle = {
+  textDecoration: 'line-through',
+  color: 'gray',
+}
+
+const addItem = (item: any) => {
+  itemList.value.push(item)
+}
+
+const deleteItem = (index: number) => {
+  itemList.value.splice(index, 1)
+}
 </script>
 
 <template>
-  <main>
-    <TodoList />
-  </main>
+  <div class="space-y-3 p-10">
+    <div class="text-4xl font-bold">To Do List</div>
+    <TodoSimpleForm @add-item="addItem" />
+    <template
+      v-if="itemList.length !== 0"
+      v-for="(item, index) in itemList"
+      :key="index"
+    >
+      <div
+        class="border p-3 rounded-md text-sm flex justify-between items-center"
+      >
+        <div class="space-x-1 flex items-center w-full">
+          <input type="checkbox" v-model="item.completed" />
+          <label :style="item.completed ? itemStyle : ''"
+            >{{ item.content }}
+          </label>
+        </div>
+        <button
+          type="button"
+          style="background-color: gray; color: white; border-radius: 5px"
+          class="px-2"
+          @click="deleteItem(index)"
+        >
+          Delete
+        </button>
+      </div>
+    </template>
+    <template v-else>
+      <div>To Do List가 없습니다!</div>
+    </template>
+  </div>
 </template>
